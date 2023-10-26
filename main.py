0d30a6ad3be402815ace44183637583be77f6d6e
lilih <lili.holler@gmail.com>
2023-10-26 14:32:25 -0400

    debugging

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ def openai_endpoint():
         openai.api_key=openai_api_key
         #response = requests.post(url=completions_endpoint, headers=headers, json=data)
         response = openai.ChatCompletion.create(model=data["model"], messages=[{'role': 'system',
-                                                                                'content': "You are an OCD therapist. Give the user an ERP technique"},
-
+                                                                                'content': "Analyse this sentence"},
                                                                                {'role': 'user', 'content': 'I am afraid I hit spmeone w my car'}])
         print("aftr response")
         print(str(response))
