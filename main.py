0cd063ee75a146bd3b934339d1bde075acde6824
lilih <lili.holler@gmail.com>
2023-10-26 09:33:28 -0400

    print env for testing

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import openai
 import os
 
 app = Flask(__name__)
-openai_api_key = os.environ.get('openai')  # Get your OpenAI API key from an environment variable
+openai_api_key = os.getenv('openai')  # Get your OpenAI API key from an environment variable
 
 @app.route('/openai', methods=['POST'])
 def openai_endpoint():
+    print(openai_api_key)
     data = request.get_json()
     # Extract necessary data from the request
     prompt = data.get('prompt')
