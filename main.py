c709feb58a8ffce68bbf98f51f86308198d6057f
lilih <lili.holler@gmail.com>
2023-10-26 09:41:15 -0400

    added logging

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 from flask import Flask, request, jsonify
 import openai
 import os
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
-openai_api_key = os.getenv('openai')  # Get your OpenAI API key from an environment variable
 
 @app.route('/openai', methods=['POST'])
 def openai_endpoint():
-    print(openai_api_key)
     data = request.get_json()
+    logging.info('Received data: %s', data)  # Log the received data
+
     # Extract necessary data from the request
     prompt = data.get('prompt')
     model = data.get('model', 'gpt-3.5-turbo')
     max_tokens = data.get('maxTokens', 1000)
     temperature = data.get('temperature', 1)
 
-    openai.api_key = openai_api_key
+    logging.info('Using model: %s, max_tokens: %d, temperature: %.1f', model, max_tokens, temperature)  # Log the extracted values
+
+    openai.api_key = os.getenv('openai')
+
     try:
-        response = openai.ChatCompletion.create(engine=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
-        return jsonify(response['choices'][0]['text'].strip())
+        response = openai.Completion.create(
+            engine=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
+        generated_text = response['choices'][0]['text'].strip()
+        logging.info('Generated text: %s', generated_text)  # Log the generated text
+        return jsonify({'response': generated_text})
     except Exception as e:
+        logging.exception('An error occurred: %s', e)  # Log exceptions with stack trace
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
